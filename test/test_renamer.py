01a3a1d523cc0296cb402320c245798cb2fce458
Sal <jorgen.sater@gmail.com>
2022-02-08 20:36:21 +1100

    Added test_file_match_regex

--- test/test_renamer.py
+++ test/test_renamer.py
@@ -14,3 +14,15 @@ import renamer
 ])
 def test_file_match(filename, filefilter, expected):
     assert(renamer.file_match(filename, filefilter)) == expected
+
+@pytest.mark.parametrize("filename, filefilter, expected", [
+    ('a', r'\w', True),
+    ('d¨1', r'\d', True),
+    ('d¨1', r'¨1{d, 2}', False),
+    ('3t32feqwgq32', '32feqq', False),
+    ('abc', 'abcd', False),
+    ('saongoaiemg,2.353ni2n36o..', '.+', True),
+    ('saongoaiemg.+,2.353ni2n36o..', r'\.\+', True),
+])
+def test_file_match_regex(filename, filefilter, expected):
+    assert(renamer.file_match(filename, filefilter)) == expected

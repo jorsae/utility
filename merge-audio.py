ed13b2b6b96271952aab51393954e7f1f50cd6f9
Sara <jorgen.sater@gmail.com>
2024-01-07 01:06:31 +1100

    Fixed merge-audio.py

--- merge-audio.py
+++ merge-audio.py
@@ -6,15 +6,22 @@ import os
 files = os.listdir()
 files.remove(sys.argv[0])
 
-index = 0
-fileconcat = ''
+i = 0
+fileconcat = []
 filterconcat = ''
 for file in files:
-    fileconcat += f'-i {file} '
-    filterconcat += f'[{index}:a]'
-    index += 1
-fileconcat = fileconcat[:-1]
+    fileconcat.append('-i')
+    fileconcat.append(file)
+    filterconcat += f'[{i}:a]'
+    i += 1
 filterconcat += f'concat=n={len(files)}:v=0:a=1[outa]'
+sys_string = f"ffmpeg -filter_complex {filterconcat} -map [outa] output_{str(datetime.now()).replace(':', '-').replace(' ', '')}.m4a"
 
-sys_string = f"ffmpeg {fileconcat} -filter_complex {filterconcat} -map [outa] {str(datetime.now()).replace(':', '-').replace(' ', '')}.m4a"
-subprocess.call(sys_string.split(' '))
+process_call = sys_string.split(' ')
+index = 1
+for fconcat in fileconcat:
+    process_call.insert(index, fconcat)
+    index += 1
+
+print(process_call)
+subprocess.call(process_call)

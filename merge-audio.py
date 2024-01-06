953264f0dce35ff5887d25fbd3e5c2412974419a
Sara <jorgen.sater@gmail.com>
2024-01-07 01:26:18 +1100

    Fixed order on input files in merge-audio

--- merge-audio.py
+++ merge-audio.py
@@ -9,6 +9,7 @@ files.remove(sys.argv[0])
 i = 0
 fileconcat = []
 filterconcat = ''
+files.sort()
 for file in files:
     fileconcat.append('-i')
     fileconcat.append(file)
@@ -16,8 +17,8 @@ for file in files:
     i += 1
 filterconcat += f'concat=n={len(files)}:v=0:a=1[outa]'
 sys_string = f"ffmpeg -filter_complex {filterconcat} -map [outa] output_{str(datetime.now()).replace(':', '-').replace(' ', '')}.m4a"
-
 process_call = sys_string.split(' ')
+
 index = 1
 for fconcat in fileconcat:
     process_call.insert(index, fconcat)
